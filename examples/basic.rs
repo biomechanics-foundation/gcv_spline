4902da548668bb8b444b70a606a25d85b906c4f6
stwilliams333 <stwilliamsmail@gmail.com>
2024-01-25 22:51:08 -0600

    Update basic.rs

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,10 +1,13 @@
-use gcv_spline::{GcvSpline, FittingError};
+use gcv_spline::GcvSpline;
 
 fn main() {
     let vec: Vec<f64> = vec![5., 6., 2., 4., 3., 6., 8., 9.];
     let time: Vec<f64> = vec![0., 1., 2., 3., 4., 5., 6., 7.];
 
     let spline = GcvSpline::from_data(&time, &vec).unwrap();
-    let evaluated: Vec<f32> = spline.points(&time).iter().map(|e| *e as f32).collect();
-    dbg!(evaluated[1] as f64);
+
+    let new_time = vec![0., 2.5, 3., 7.];
+    let evaluated = spline.points(&new_time);
+
+    dbg!(evaluated);
 }
